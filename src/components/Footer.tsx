c45705db0a3a55d8719052b7a4930e84b2a8d1f2
Wael Mejdoub <wael.mejdoub.dev@gmail.com>
2023-03-18 11:53:47 +0100

    create Navbar, Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+
+    return (
+      <>
+        <main>
+          <ul>
+              <li>Instagram</li>
+              <li>Facebook</li>
+              <li>twitter</li>
+          </ul>
+        </main>
+      </>
+    )
+  }
+  
+  export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+
+  return (
+    <>
+      <main>
+        <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>contact</li>
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export default Navbar
